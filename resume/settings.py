3564ed8f55b9bfea3757569eb89e7b92176fb068
dennocui <33720881+Dennocui@users.noreply.github.com>
2023-03-08 13:09:34 +0300

    Remove postgres for sqlite

--- resume/settings.py
+++ resume/settings.py
@@ -90,24 +90,24 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'resume',
-        'USER': 'postgres',
-        'PASSWORD': 'times',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'resume',
+#         'USER': 'postgres',
+#         'PASSWORD': 'times',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+
+#     }
+# }
 
 
 # db_from_env = dj_database_url.config(conn_max_age=600)
